2a5a6032d8de69262543ae38724d49492a1c780f
Marcus <marek17.10.2001@gmail.com>
2022-05-30 17:06:09 +0200

    Foundations for the countdown

--- js/index.js
+++ js/index.js
@@ -2,35 +2,68 @@ const btn1 = document.querySelector(".start");
 const btn = document.querySelector("button");
 
 
-const hours = document.querySelector("#hours").value;
+
 let countdown = () => {
+let hours = Number(document.querySelector("#hours").value);
 let minutes = Number(document.querySelector("#minutes").value);
-let seconds = document.querySelector("#seconds").value;
-  let minutess = minutes * 60;
-  console.log(minutess);
-//console.log(seconds);
+let seconds = Number(document.querySelector("#seconds").value);
+
+let result = hours*3600 + minutes*60 + seconds;
+  
  let x = setInterval(function () {
-     
+ hours = parseInt(result / 3600, 10);
+ minutes = parseInt((result/60)-hours*60);
+ seconds = parseInt(result % 60, 10);
+   // console.log("H: " + hours);
+   //console.log(minutes);
+   //console.log(seconds);
+   hours = hours < 10 ? "0" + hours : hours;
+ minutes = minutes < 10 ? "0" + minutes : minutes;
+   seconds = seconds < 10 ? "0" + seconds : seconds;
+   // if(seconds < 0) {
+   //   seconds += 60;
+   //  // minutes--;
+   // }
+   //console.log('h');
+     document.querySelector(".display-time-hours").innerHTML = hours + ":";
+    document.querySelector(".display-time-minutes").innerHTML = minutes + ":";
+    document.querySelector(".display-time").innerHTML = seconds;
+    //console.log("Cały wynik: " + result--); 
+     if (--result < 0) {
+            clearInterval(x);
+        }
    
-   // if(minutess == 0) {
-   //   console.log("To koniec");
-   //   clearInterval(x);
+   
+   //console.log(seconds--);
+
+   // if(result % 60 == 0) {
+   //  document.querySelector(".display-time-minutes").innerHTML = "<p>" + minutes-- + " min</p>";
+   // } else {
+   //    document.querySelector(".display-time-minutes").innerHTML = "<p>" + minutes + " min</p>";
+   //    document.querySelector(".display-time").innerHTML = "<p>" + seconds--  + " sec</p>";
    // }
-   // else {
-   //   document.querySelector(".display.time").innerHTML = minutess--;
-   // }
-   if(seconds < 0){
-     clearInterval(x);
-   } else {
-   document.querySelector(".display-time").innerHTML = seconds--;
-    }
+  
+  //  if(minutess < 0) {
+  //    console.log("To koniec");
+  //    clearInterval(x);
+  //  }
+  //  else {
+  //    if(minutess % 60 == 0){
+  //    document.querySelector(".display-time-minutes").innerHTML = "<p>" + minutes-- + " min</p>";
+  //    console.log("tez");
+  //  }
+  //    console.log(minutess--);
+  // }
+  //  if(seconds < 0){
+  //    //clearInterval(x);
+  //  } else {
+  //  document.querySelector(".display-time").innerHTML = "<p>" + seconds--  + " sec</p>";
+  //   }
     
      
-btn.addEventListener('click',function(){
-  clearInterval(x);
-});
-  }, 1000);
-}
 
+  }, 1000);
+
+}
 btn1.addEventListener('click', countdown);
 
