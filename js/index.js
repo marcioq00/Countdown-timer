2d10246711f24d4e21081f06a51bcde2f48854fb
Marcus <marek17.10.2001@gmail.com>
2022-06-07 00:15:41 +0200

    I added a circle animation to the time running out

--- js/index.js
+++ js/index.js
@@ -1,17 +1,53 @@
+
+
 const start = document.querySelector(".start");
 start.addEventListener("click", function () {
+  const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green"
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD
+  }
+};
+
+
+
   const btnPause = document.querySelector(".pause");
   const btnResume = document.querySelector(".resume");
   const btnStop = document.querySelector(".stop");
   const userHours = Number(document.querySelector(".hours").value);
   const userMinutes = Number(document.querySelector(".minutes").value);
   const userSeconds = Number(document.querySelector(".seconds").value);
-  let result = userHours * 60 + userMinutes + userSeconds / 60;
+  const result = userHours * 60 + userMinutes + userSeconds / 60;
   let current_time = Date.parse(new Date());
+ 
   let deadline = new Date(current_time + result * 60 * 1000);
+  
+  
+    let a = Date.parse(deadline);
+    let b = Date.parse(new Date());
+    let TIME_LIMIT = (a - b)/1000;
+    console.log(TIME_LIMIT/1000);
+
+    let timePassed = 0;
+let timeLeft = TIME_LIMIT;
+let timerInterval = null;
+let remainingPathColor = COLOR_CODES.info.color;
+
 
   function time_remaining(endtime) {
-    let t = Date.parse(endtime) - Date.parse(new Date());
+    const t = Date.parse(endtime) - Date.parse(new Date());
+    console.log("stala t: "+  t);
     let seconds = Math.floor((t / 1000) % 60);
     let minutes = Math.floor((t / 1000 / 60) % 60);
     let hours = Math.floor((t / (1000 * 60 * 60)) % 24);
@@ -25,9 +61,36 @@ start.addEventListener("click", function () {
       t.hours = t.hours < 10 ? "0" + t.hours : t.hours;
       t.minutes = t.minutes < 10 ? "0" + t.minutes : t.minutes;
       t.seconds = t.seconds < 10 ? "0" + t.seconds : t.seconds;
-      document.querySelector(".display-time-hours").innerHTML = t.hours + ":";
-      document.querySelector(".display-time-minutes").innerHTML = t.minutes + ":";
-      document.querySelector(".display-time").innerHTML = t.seconds;
+      // document.querySelector(".display-time-hours").innerHTML = t.hours + ":";
+      // document.querySelector(".display-time-minutes").innerHTML = t.minutes + ":";
+      // document.querySelector(".display-time").innerHTML = t.seconds;
+      document.getElementById("app").innerHTML = `
+<div class="base-timer">
+  <svg class="base-timer__svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+    <g class="base-timer__circle">
+      <circle class="base-timer__path-elapsed" cx="50" cy="50" r="45"></circle>
+      <path
+        id="base-timer-path-remaining"
+        stroke-dasharray="283"
+        class="base-timer__path-remaining ${remainingPathColor}"
+        d="
+          M 50, 50
+          m -45, 0
+          a 45,45 0 1,0 90,0
+          a 45,45 0 1,0 -90,0
+        "
+      ></path>
+    </g>
+  </svg>
+  <span id="base-timer-label" class="base-timer__label">${t.hours + t.minutes + t.seconds}</span>
+</div>
+`;
+      timePassed = timePassed += 1;
+      console.log(TIME_LIMIT);
+      timeLeft = TIME_LIMIT - timePassed;
+      document.getElementById("base-timer-label").innerHTML = t.hours + ":" +t.minutes + ":" + t.seconds;
+      setCircleDasharray();
+      setRemainingPathColor(timeLeft);
       if (t.total <= 0) {
         clearInterval(timeinterval);
       }
@@ -47,7 +110,38 @@ start.addEventListener("click", function () {
 
   let paused = false;
   let time_left;
-
+  function setRemainingPathColor(timeLeft) {
+    const { alert, warning, info } = COLOR_CODES;
+    if (timeLeft <= alert.threshold) {
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(warning.color);
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(alert.color);
+    } else if (timeLeft <= warning.threshold) {
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.remove(info.color);
+      document
+        .getElementById("base-timer-path-remaining")
+        .classList.add(warning.color);
+    }
+  }
+  
+  function calculateTimeFraction() {
+    const rawTimeFraction = timeLeft / TIME_LIMIT;
+    return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+  }
+  
+  function setCircleDasharray() {
+    const circleDasharray = `${(
+      calculateTimeFraction() * FULL_DASH_ARRAY
+    ).toFixed(0)} 283`;
+    document
+      .getElementById("base-timer-path-remaining")
+      .setAttribute("stroke-dasharray", circleDasharray);
+  }
   function pauseClock() {
     if (!paused) {
       paused = true;
@@ -69,38 +163,3 @@ start.addEventListener("click", function () {
   btnPause.addEventListener("click", pauseClock);
   btnResume.addEventListener("click", resumeClock);
 });
-
-
-
-// const btnStart = document.querySelector(".start");
-// const btnStop = document.querySelector(".stop");
-
-// let countdown = () => {
-//   let hours = Number(document.querySelector("#hours").value);
-//   let minutes = Number(document.querySelector("#minutes").value);
-//   let seconds = Number(document.querySelector("#seconds").value);
-
-//   let result = hours * 3600 + minutes * 60 + seconds;
-
-//   let x = setInterval(function () {
-//     hours = parseInt(result / 3600, 10);
-//     minutes = parseInt(result / 60 - hours * 60);
-//     seconds = parseInt(result % 60, 10);
-//     hours = hours < 10 ? "0" + hours : hours;
-//     minutes = minutes < 10 ? "0" + minutes : minutes;
-//     seconds = seconds < 10 ? "0" + seconds : seconds;
-
-//     document.querySelector(".display-time-hours").innerHTML = hours + ":";
-//     document.querySelector(".display-time-minutes").innerHTML = minutes + ":";
-//     document.querySelector(".display-time").innerHTML = seconds;
-
-//     if (--result < 0) {
-//       clearInterval(x);
-//     }
-//     btnStop.addEventListener("click", function () {
-//       clearInterval(x);
-//     });
-//   }, 1000);
-// };
-
-// btnStart.addEventListener("click", countdown);
